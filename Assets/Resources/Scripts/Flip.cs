aa8ef6ce4dc6a05e5caf6052285cd66d85201d73
Dounat14 <dominik.braungardt.00@gmx.de>
2022-06-18 10:46:25 +0200

    Punkte(fix)

--- Assets/Resources/Scripts/Flip.cs
+++ Assets/Resources/Scripts/Flip.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Flip : MonoBehaviour
 {
     public GameObject Bat;
+    public bool first = false;
+    public Material color;
     public void Vorne()
     {
         Bat.transform.Rotate(0, -180, 0);
--- Assets/Resources/Scripts/Punktezähler.cs
+++ Assets/Resources/Scripts/Punktezähler.cs
@@ -21,42 +21,146 @@ public class Punktezähler : MonoBehaviour
     {
         if (collision.gameObject.name == "table1")
         {
+            tisch11++;
             tisch1 = true;
             if (first && bat1)
             {
                 Player2++;
+                bat1 = false;
+                tisch1 = false;
+                tisch2 = false;
+                bat2 = false;
+                floor = false;
+                first = false;
+                tisch22 = 0;
+                tisch11 = 0;
+                transform.position = new Vector3(2, (float)0.2, (float)-0.015);
+                GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
             }
             if (!first && bat1)
             {
                 first = true;
             }
-            tisch11++;
+            if (!first && bat2)
+            {
+                Player1++;
+                bat1 = false;
+                tisch1 = false;
+                tisch2 = false;
+                bat2 = false;
+                floor = false;
+                first = false;
+                tisch22 = 0;
+                tisch11 = 0;
+                transform.position = new Vector3(-2, (float)0.2, (float)-0.015);
+                GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+            }
             if (tisch11 > 1 && (bat1 || bat2))
             {
                 Player2++;
+                bat1 = false;
+                tisch1 = false;
+                tisch2 = false;
+                bat2 = false;
+                floor = false;
+                first = false;
+                tisch22 = 0;
+                tisch11 = 0;
+                transform.position = new Vector3(2, (float)0.2, (float)-0.015);
+                GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
             }
         }
 
         if (collision.gameObject.name == "table2")
         {
+            tisch22++;
             tisch2 = true;
             if (first && bat2)
             {
                 Player1++;
+                bat1 = false;
+                tisch1 = false;
+                tisch2 = false;
+                bat2 = false;
+                floor = false;
+                first = false;
+                tisch22 = 0;
+                tisch11 = 0;
+                transform.position = new Vector3(-2, (float)0.2, (float)-0.015);
+                GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
             }
             if (!first && bat2)
             {
                 first = true;
             }
-            tisch22++;
+            if (!first && bat1)
+            {
+                Player2++;
+                bat1 = false;
+                tisch1 = false;
+                tisch2 = false;
+                bat2 = false;
+                floor = false;
+                first = false;
+                tisch22 = 0;
+                tisch11 = 0;
+                transform.position = new Vector3(2, (float)0.2, (float)-0.015);
+                GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+            }
             if (tisch22 > 1 && (bat1 || bat2))
             {
                 Player1++;
+                bat1 = false;
+                tisch1 = false;
+                tisch2 = false;
+                bat2 = false;
+                floor = false;
+                first = false;
+                tisch22 = 0;
+                tisch11 = 0;
+                transform.position = new Vector3(-2, (float)0.2, (float)-0.015);
+                GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
             }
         }
         if (collision.gameObject.name == "Bat1")
         {
-
+            bat1 = true;
+            if (first && !tisch1 && !tisch2)
+            {
+                Player2++;
+                bat1 = false;
+                first = false;
+                transform.position = new Vector3(2, (float)0.2, (float)-0.015);
+                GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+            }
+            floor = false;
+            tisch1 = false;
+            tisch2 = false;
+            bat2 = false;
+            tisch22 = 0;
+            tisch11 = 0;
+        }
+        if (collision.gameObject.name == "Bat2")
+        {
+            bat2 = true;
+            if (first && !tisch1 && !tisch2)
+            {
+                Player1++;
+                bat2 = false;
+                first = false;
+                transform.position = new Vector3(-2, (float)0.2, (float)-0.015);
+                GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+            }
+            floor = false;
+            tisch1 = false;
+            tisch2 = false;
+            bat1 = false;
+            tisch22 = 0;
+            tisch11 = 0;
+        }
+        if (collision.gameObject.name == "Floor")
+        {
+            floor = true;
         }
     }
 
@@ -67,6 +171,63 @@ public class Punktezähler : MonoBehaviour
 
     private void Punkte()
     {
-
+        //Erfolgreicher Schlag auf zweite platte nach Schlag 
+        if (bat1 && first && tisch2 && floor)
+        {
+            Player1++;
+            bat1 = false;
+            tisch1 = false;
+            tisch2 = false;
+            bat2 = false;
+            floor = false;
+            first = false;
+            tisch22 = 0;
+            tisch11 = 0;
+            transform.position = new Vector3(-2, (float)0.2, (float)-0.015);
+            GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+        }
+        if (bat2 && first && tisch1 && floor)
+        {
+            Player2++;
+            bat1 = false;
+            tisch1 = false;
+            tisch2 = false;
+            bat2 = false;
+            floor = false;
+            first = false;
+            tisch22 = 0;
+            tisch11 = 0;
+            transform.position = new Vector3(2, (float)0.2, (float)-0.015);
+            GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+        }
+        //Spielfehler ohne Platte zu treffen
+        if (bat1 && floor)
+        {
+            Player2++;
+            bat1 = false;
+            tisch1 = false;
+            tisch2 = false;
+            bat2 = false;
+            floor = false;
+            first = false;
+            tisch22 = 0;
+            tisch11 = 0;
+            transform.position = new Vector3(2, (float)0.2, (float)-0.015);
+            GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+        }
+        if (bat2 && floor)
+        {
+            Player1++;
+            bat1 = false;
+            tisch1 = false;
+            tisch2 = false;
+            bat2 = false;
+            floor = false;
+            first = false;
+            tisch22 = 0;
+            tisch11 = 0;
+            transform.position = new Vector3(-2, (float)0.2, (float)-0.015);
+            GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+        }
     }
 }
